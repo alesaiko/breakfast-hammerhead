97ee46bdf96dcc5c9555621cd26232374dfcd5dd
Suman Tatiraju <sumant@codeaurora.org>
2018-02-04 12:54:08 +0300

    msm: kgsl: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Change-Id: I4061aff8a44ef697245faa4827c3cb1019f781da
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -499,7 +499,6 @@ static int kgsl_pwrctrl_idle_timer_store(struct device *dev,
 	unsigned long val;
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
 	struct kgsl_pwrctrl *pwr;
-	const long div = 1000/HZ;
 	int rc;
 
 	if (device == NULL)
@@ -515,8 +514,7 @@ static int kgsl_pwrctrl_idle_timer_store(struct device *dev,
 	mutex_lock(&device->mutex);
 
 	/* Let the timeout be requested in ms, but convert to jiffies. */
-	val /= div;
-	pwr->interval_timeout = val;
+	pwr->interval_timeout = msecs_to_jiffies(val);
 
 	mutex_unlock(&device->mutex);
 
@@ -528,12 +526,11 @@ static int kgsl_pwrctrl_idle_timer_show(struct device *dev,
 					char *buf)
 {
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
-	int mul = 1000/HZ;
 	if (device == NULL)
 		return 0;
 	/* Show the idle_timeout converted to msec */
 	return snprintf(buf, PAGE_SIZE, "%d\n",
-		device->pwrctrl.interval_timeout * mul);
+		jiffies_to_msecs(device->pwrctrl.interval_timeout));
 }
 
 static int kgsl_pwrctrl_pmqos_latency_store(struct device *dev,
@@ -1091,7 +1088,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 	pwr->power_flags = 0;
 
 	pwr->idle_needed = pdata->idle_needed;
-	pwr->interval_timeout = pdata->idle_timeout;
+	pwr->interval_timeout = msecs_to_jiffies(pdata->idle_timeout);
 	pwr->strtstp_sleepwake = pdata->strtstp_sleepwake;
 	pwr->ebi1_clk = clk_get(&pdev->dev, "bus_clk");
 	if (IS_ERR(pwr->ebi1_clk))
@@ -1642,7 +1639,8 @@ int kgsl_active_count_wait(struct kgsl_device *device, int count)
 		int ret;
 		mutex_unlock(&device->mutex);
 		ret = wait_event_timeout(device->active_cnt_wq,
-			_check_active_count(device, count), HZ);
+			_check_active_count(device, count),
+				msecs_to_jiffies(1000));
 		mutex_lock(&device->mutex);
 		result = ret == 0 ? -ETIMEDOUT : 0;
 	}
